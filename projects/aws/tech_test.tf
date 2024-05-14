f792971264e43dcf9c2a331f729c0435ccd7e5b1
Kevin Perillo <kevperillo@gmail.com>
2024-05-14 13:51:57 -0700

    lambda added, terraform working, python code not tested

--- projects/aws/tech_test.tf
+++ projects/aws/tech_test.tf
@@ -5,3 +5,9 @@ module "vpc" {
 module "s3" {
   source = "../../modules/s3"
 }
+
+module "lambda" {
+  source = "../../modules/lambda"
+  tech_test_bucket_arn = module.s3.tech_test_bucket_arn
+  tech_test_bucket_id  = module.s3.tech_test_bucket_id
+}
